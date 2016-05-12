900a8f4734d6c3fa8c3397bc3c39412830054ae2
Cameron Kleiman <ckleiman15@gmail.com>
2016-05-12 15:11:08 -0500

    Messed with buttons
    Yep. Just added buttons. And alerts. Buttons and alerts. And popups.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
         $("#main").animate({top:'100px'},1700,'easeInOutQuint');
+        $('.btn-danger').click(function() {
+          $('#alertdos').toggleClass("alert-success");
+          $('#alertdos').toggleClass("alert-danger");
+        });
+        $('.btn-default').click(function() {
+          alert('Told you so.');
+        })
 });
