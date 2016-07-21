30ef35749c5e9840ea83314dac1f9582bde189c0
Cameron Kleiman <ckleiman15@gmail.com>
2016-07-20 20:12:01 -0400

    Add info to nerd problems button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        $("#main").animate({top:'100px'},1700,'easeInOutQuint');
+    $("#main").animate({ top: '100px' }, 1700, 'easeInOutQuint');
         $('#resumebutton').click(function() {
           $('#alertdos').toggleClass("alert-success");
           $('#alertdos').toggleClass("alert-danger");
@@ -12,4 +12,12 @@ $(document).ready(function(){
             $('#arrowicon').toggleClass("fa-arrow-circle-down");
             $('.panel-body').slideToggle({duration: 750, easing: 'easeOutExpo'});
         })
+        $("#bloginfo").popover({
+            animation: true,
+            content: "A blog by Cameron Kleiman about various problems encountered by nerds on an impressively frequent basis, but also other things",
+            title: "About Nerd Problems",
+            trigger: "hover click",
+            placement: "auto right",
+            container: "body"
+        });
 });
